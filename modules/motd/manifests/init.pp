b7c19d2f856963e8afa9e333e3272175bf123b9a
root <root@ip-172-31-51-240.ec2.internal>
2017-07-11 18:39:49 -0400

    fixed

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -1,11 +1,13 @@
 class motd {
 
-  $hostname = $facts['networking']['fqdn']
-
+  $hostname   = $facts['networking']['fqdn']
+  $os_name    = $facts['os']['name']
+  $os_release = $facts['os']['release']
 
   File { '/etc/motd':
-    path   =>  '/etc/motd',
-   ensure  =>  file,
-   source  =>  'puppet:///modules/motd/motd',
+    path    =>  '/etc/motd',
+    ensure  =>  file,
+    #source  =>  'puppet:///modules/motd/motd',
+    content  =>  "\n\n[Puppet] ${hostname} ${os_name} $os_release}\n\n",
  }
 }
